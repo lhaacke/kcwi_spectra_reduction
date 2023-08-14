68e0f9c833ca955ad14a6255adf8c912dcacd7d1
lhaacke <lydia.haacke@gmail.com>
2023-08-14 16:33:04 +1000

    changed ppxf_fit_kinematics to work for MUSE data

--- ppxf_fit_kinematics.py
+++ ppxf_fit_kinematics.py
@@ -16,23 +16,20 @@ def vac_to_air(lam_vac):
     lam_air = lam_vac / (1.0 + 2.735182e-4 + 131.4182 / lam_vac**2 + 2.76249e8 / lam_vac**4)
     return lam_air
 
-def fit_vel_sigma(spectrum, save_as, z, grating, shift_spec=True, cut_spec=False, fit=False, bootstrap=False, smoothed_spec=''):
+def fit_vel_sigma(spectrum, save_as, z, shift_spec=True, fit=False, bootstrap=False, smoothed_spec='', fit_z=False):
     '''
-    currently working for KCWI spectra (and specifically for swinburne or yale observed of NGC5846_UDG1)
+    fit spectra from a muse cube
     spectrum: fits file with spectrum to fit
     save_as: if fit==True - txt file to write result to
             if bootstrap==True - fits file to write result to
     z: initial redshift guess
-    grating: (which KCWI grating was used, which uni observed)
     shift_spec: shift spectrum to redshift zero before fitting if True
-    cut_spec: remove wavelengths outside of wavgood
     fit: fit multiple combinations of degree, mdegree
     bootstrap: fit 10 times with 10 different parts of the spectrum masked
     smoothed_spec: smoothed spectrum of same shape as spectrum for plotting
     '''
 
     ppxf_dir = path.dirname(path.realpath(util.__file__))
-    print('I WAS IN CODES')
 
     ################################ PROCESS GALAXY SPECTRUM ##############################
     # read the spectrum and define the wavelength range to fit
@@ -44,19 +41,11 @@ def fit_vel_sigma(spectrum, save_as, z, grating, shift_spec=True, cut_spec=False
     nan_check = np.isnan(gal_lin)
     gal_lin[nan_check] = 0.
 
-    lamRange1 = np.array([h1['WAVALL0'], h1['WAVALL1']])
+    lamRange1 = np.array([h1['WAVELMIN']*10, h1['WAVELMAX']*10])
+    # fwhm_gal = np.average(lamRange1/3000)
+    fwhm_gal = 2.75
 
-    if grating[0]=='BH3_Medium' and grating[1]=='swin':
-        fwhm_gal = np.average(lamRange1/9000)
-    elif grating[0]=='BH3_Large' and grating[1]=='swin':
-        fwhm_gal = np.average(lamRange1/4500)
-    elif grating[0]=='BH3_Large' and grating[1]=='yale':
-        fwhm_gal = np.average(lamRange1/4500)
-    elif grating[0]=='BL_Large' and grating[1]=='swin':
-        fwhm_gal = np.average(lamRange1/900)
-    elif grating[0]=='BL_Large' and grating[1]=='yale':
-        fwhm_gal = np.average(lamRange1/900)
-    print('{}, {}, fwhm_gal:{}'.format(grating[0], grating[1], fwhm_gal))
+    print('fwhm_gal:{}'.format(fwhm_gal))
 
     # redshift adjustment if necessary
     # controlled by bool shift_spec, default True
@@ -70,32 +59,36 @@ def fit_vel_sigma(spectrum, save_as, z, grating, shift_spec=True, cut_spec=False
         redshift = redshift_0
 
     galaxy, ln_lam1, velscale = util.log_rebin(lamRange1, gal_lin)
+    # check for infinite pixels
+    c_inf = np.isfinite(galaxy)
+    galaxy[~c_inf] = np.nanmedian(galaxy)
     galaxy = galaxy/np.median(galaxy)  # Normalize spectrum to avoid numerical issues
     noise = np.full_like(galaxy, 1)           # Assume constant noise per pixel here
 
 
     ################################# GET THE TEMPLATES #####################################
-    coelho = glob.glob(ppxf_dir + '/s_coelho14_highres/*.fits')
-    # FWHM_tem = 2.51     # Vazdekis+16 spectra have a constant resolution FWHM of 2.51A.
+    # coelho = glob.glob(ppxf_dir + '/s_coelho14_highres/*.fits')
+    miles = glob.glob(ppxf_dir + '/miles_models/*.fits')
+    fwhm_tem = 2.51     # Vazdekis+16 spectra have a constant resolution FWHM of 2.51A.
     velscale_ratio = 2  # adopts 2x higher spectral sampling for templates than for galaxy
 
-    hdu = fits.open(coelho[0])
+    hdu = fits.open(miles[0])
     ssp = hdu[0].data
     h2 = hdu[0].header
     lam2 = h2['CRVAL1'] + h2['CDELT1']*np.arange(h2['NAXIS1'])
-    fwhm_tem = np.average(lam2/20000) # R=20000 for coelho library
+    # fwhm_tem = np.average(lam2/20000) # R=20000 for coelho library
 
     good_lam = (lam2 > lamRange1[0]/1.02) & (lam2 < lamRange1[1]*1.02)
     ssp, lam2 = ssp[good_lam], lam2[good_lam]
 
     lamRange2 = [np.min(lam2), np.max(lam2)]
     sspNew, ln_lam2 = util.log_rebin(lamRange2, ssp, velscale=velscale/velscale_ratio)[:2]
-    templates = np.empty((sspNew.size, len(coelho)))
+    templates = np.empty((sspNew.size, len(miles)))
 
     fwhm_dif = np.sqrt(fwhm_gal**2 - fwhm_tem**2)
     sigma = fwhm_dif/2.355/h2['CDELT1']  # Sigma difference in pixels
 
-    for j, file in enumerate(coelho):
+    for j, file in enumerate(miles):
         hdu = fits.open(file)
         ssp = hdu[0].data
         ssp = ndimage.gaussian_filter1d(ssp[good_lam], sigma)
@@ -103,35 +96,11 @@ def fit_vel_sigma(spectrum, save_as, z, grating, shift_spec=True, cut_spec=False
         templates[:, j] = sspNew/np.median(sspNew[sspNew > 0])  # Normalizes templates
 
     ############################### MASK PIXELS ############################################
-    if cut_spec:
-        print('spectrum cut')
-        n_pix_left = int((h1['WAVGOOD0']-h1['WAVALL0']) / h1['CDELT1'] + 5)
-        n_pix_right = int((h1['WAVALL1']-h1['WAVGOOD1']) / h1['CDELT1'] + 5)
-        mask = np.full_like(galaxy, 1)
-        mask[:n_pix_left] = 0
-        mask[(len(galaxy)-n_pix_right):] = 0
-        goodPixels = np.flatnonzero(mask)
-    elif bootstrap:
-        print('making masks for bootstrap error')
-        bootstrap_masks = np.ones(shape=(10, len(galaxy)))
-        goodPixels = []
-        # masking edges for all of theme
-        n_pix_left = int((h1['WAVGOOD0']-h1['WAVALL0']) / h1['CDELT1'] + 5)
-        n_pix_right = int((h1['WAVALL1']-h1['WAVGOOD1']) / h1['CDELT1'] + 5)
-        mask_frac = [(0, .1), (.1, .2), (.2, .3), (.3, .4), (.4, .5), (.5, .6), (.6, .7), (.7, .8), (.8, .9), (.9, 1.)]
-        for mask in enumerate(bootstrap_masks):
-            mask[1][:n_pix_left] = 0
-            mask[1][(len(galaxy)-n_pix_right):] = 0
-            mask[1][int(mask_frac[mask[0]][0]*len(galaxy)) : int(mask_frac[mask[0]][1]*len(galaxy))] = 0
-            goodPixels.append(np.flatnonzero(mask[1]))
-        print('bootstrap masks done')
-    else:
-        print('spectrum not cut')
-        mask = np.full_like(galaxy, 1)
-        mask[:10] = 0
-        mask[-10:] = 0
-        goodPixels = np.flatnonzero(mask)
-    print(len(goodPixels))
+    print('spectrum not cut')
+    mask = np.full_like(galaxy, 1)
+    mask[:10] = 0
+    mask[-10:] = 0
+    goodPixels = np.flatnonzero(mask)
 
     ######################### FIT A GC WITH DIFFERENT DEGREES, MDEGREES ##################
     c = 299792.458
@@ -144,13 +113,11 @@ def fit_vel_sigma(spectrum, save_as, z, grating, shift_spec=True, cut_spec=False
     t = clock()
 
     if fit:
-        degree = [1, 3, 5, 7, 9, 11, 13, 15]
-        mdegree = [1, 2, 3, 4, 5, 6]
-        # degree = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-        # mdegree = [5, 6, 7, 8, 9, 10, 11, 12]
-        res = np.recarray(shape = (len(degree)*len(mdegree)), # array to store the result for each degree combination in
-                dtype = [('z_ini', float), ('deg', int), ('mdeg', int), ('v', float), ('v_err', float), ('z', float), ('z_err', float), ('sig', float),
-                        ('sig_err', float), ('sn_median', float)]) # one for deg, mdeg, v, v_err, redshift, redshift_error, S/N
+        degree = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+        mdegree = [1, 2, 3, 4, 5, 6, 7, 8]
+        res = np.recarray(shape = (len(degree)*len(mdegree) + 1), # array to store the result for each degree combination in
+                dtype = [('deg', int), ('mdeg', int), ('v', float), ('v_err', float), ('z', float), ('z_err', float), ('sig', float),
+                        ('sig_err', float), ('sn_median', float), ('sn_average', float)]) # one for deg, mdeg, v, v_err, redshift, redshift_error, S/N
         i = 0
         for deg in degree: # iterate through degrees
             for mdeg in mdegree: # iterate through mdegrees
@@ -166,15 +133,18 @@ def fit_vel_sigma(spectrum, save_as, z, grating, shift_spec=True, cut_spec=False
                 redshift_best = np.exp(vtot/c) - 1          # eq.(8) Cappellari (2017)
                 errors = pp.error*np.sqrt(pp.chi2)          # Assume the fit is good
                 redshift_err = np.exp(vtot/c)*errors[0]/c   # Error propagation
-                sn_median = 2 * np.median(np.sqrt((pp.bestfit/(pp.galaxy - pp.bestfit))**2)) # signal to noise ratio median
+                sn_median = np.median(np.sqrt((pp.bestfit/(pp.galaxy - pp.bestfit))**2)) # signal to noise ratio median
+                sn_average = np.average(np.sqrt((pp.bestfit/(pp.galaxy - pp.bestfit))**2)) # signal to noise ratio average
                 # fill array with results
-                res[i] = (z, deg, mdeg, vtot, errors[0], redshift_best, redshift_err, pp.sol[1], errors[1], sn_median)
+                res[i] = (deg, mdeg, vtot, errors[0], redshift_best, redshift_err, pp.sol[1], errors[1], sn_median, sn_average)
                 i += 1
+        res[i] = (99, 99, np.average(res['v'][:-1]), np.average(res['v_err'][:-1]), np.average(res['z'][:-1]), np.average(res['z_err'][:-1]),
+                    np.average(res['sig'][:-1]), np.average(res['sig_err'][:-1]), np.average(res['sn_median'][:-1]), np.average(res['sn_average'][:-1]))
     elif bootstrap:
         # bootstrap_masks: array with different masks with different parts of the spectra masked
         # plan: run fit for each mask in the bootstrap mask array and save results same as with fit
-        deg = 5
-        mdeg = 3
+        deg = 10
+        mdeg = 4
         res = np.recarray(shape = (len(bootstrap_masks) + 1), # array to store the result for each degree combination in
                 dtype = [('deg', int), ('mdeg', int), ('v', float), ('v_err', float), ('z', float), ('z_err', float), ('sig', float),
                         ('sig_err', float), ('sn_median', float), ('sn_average', float)]) # one for deg, mdeg, v, v_err, redshift, redshift_error, S/N
@@ -202,18 +172,16 @@ def fit_vel_sigma(spectrum, save_as, z, grating, shift_spec=True, cut_spec=False
                     np.average(res['sig'][:-1]), np.average(res['sig_err'][:-1]), np.average(res['sn_median'][:-1]), np.average(res['sn_average'][:-1]))
     else:
         pp = ppxf(templates, galaxy, noise, velscale, start, goodpixels=goodPixels,
-                  plot=False, moments=2, degree=10, mdegree=6,
+                  plot=True, moments=2, degree=10, mdegree=4,
                   lam=np.exp(ln_lam1), lam_temp=np.exp(ln_lam2),
                   velscale_ratio=velscale_ratio)
 
         # The updated best-fitting redshift is given by the following
         # lines (using equation 8 of Cappellari 2017, MNRAS)
         vcosm = c*np.log(1 + redshift_0)            # This is the initial redshift estimate
-        print(vcosm)
         vpec = pp.sol[0]                            # This is the fitted residual velocity
-        print(vpec)
         vtot = vcosm + vpec                         # I add the two velocities before computing z
-        print(vtot)
+        print('vcosm: {}, vpec: {}, vtot: {}'.format(vcosm, vpec, vtot))
         if shift_spec:
             redshift_best = np.exp(vtot/c) - 1          # eq.(8) Cappellari (2017)
             errors = pp.error*np.sqrt(pp.chi2)          # Assume the fit is good
@@ -222,7 +190,7 @@ def fit_vel_sigma(spectrum, save_as, z, grating, shift_spec=True, cut_spec=False
             redshift_best = np.exp(vpec/c) - 1          # eq.(8) Cappellari (2017)
             errors = pp.error*np.sqrt(pp.chi2)          # Assume the fit is good
             redshift_err = np.exp(vpec/c)*errors[0]/c   # Error propagation
-        sn = np.median(2*np.sqrt(((pp.bestfit-pp.apoly)/(pp.galaxy - pp.bestfit))**2))
+        sn = np.nanmedian(2*np.sqrt((pp.bestfit-pp.apoly)/(pp.galaxy - pp.bestfit))**2)
 
         print("Formal errors:")
         print("     dV    dsigma   dh3      dh4")
@@ -233,12 +201,16 @@ def fit_vel_sigma(spectrum, save_as, z, grating, shift_spec=True, cut_spec=False
         print(sn)
 
     if fit:
-        with open(save_as, 'a') as f:
+        with open(save_as, 'w') as f:
             for line in res:
                 f.write(str(line))
                 f.write('\n')
     elif bootstrap:
         fits.writeto(save_as, res, overwrite=True)
+    elif fit_z:
+        plot_result(pp, save_as, redshift_best, lamRange1, smoothed_spec=smoothed_spec)
+        plt.close()
+        return vtot, redshift_best, errors, redshift_err, sn
     else:
         plot_result(pp, save_as, redshift_best, lamRange1, smoothed_spec=smoothed_spec)
         # plt.savefig(save_as)
@@ -264,17 +236,18 @@ def plot_result(pp, save_as, z_best, lamRange1, spec=True, shift_z=True, degree=
     # Plot observed spectrum
     if spec:
         ll, rr = np.min(x), np.max(x)
-        print(ll)
-        print(rr)
         resid = pp.galaxy - pp.bestfit
+        stars_bestfit = pp.bestfit
         bestfit = pp.bestfit
+        if pp.gas_any:
+            stars_bestfit = bestfit - pp.gas_bestfit
         sig3 = np.percentile(abs(resid[pp.goodpixels]), 99.73)
         ref = np.min(bestfit) - 2*sig3
         mx = np.max(bestfit) + sig3
         resid += ref                           # Offset residuals to avoid overlap
         mn = np.min(resid[pp.goodpixels])    # Plot all fitted residuals without clipping
 
-        fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(18, 12))
+        fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(25, 10))
         plt.subplots_adjust(hspace=.0)
 
         # plot the galaxy and goodPixels
@@ -339,35 +312,35 @@ def plot_result(pp, save_as, z_best, lamRange1, spec=True, shift_z=True, degree=
 
         if shift_z:
             lines_zshift = lines * (1 + z_best) # lines redshifted
-            ax[1].axvline(lines_zshift[0], c='blue')
-            ax[1].axvline(lines_zshift[1], c='red')
-            ax[1].axvline(lines_zshift[2], c='red')
-            ax[1].axvline(lines_zshift[3], c='red')
-            ax[1].axvline(lines_zshift[5], c='m')
-            ax[1].axvline(lines_zshift[-2], c='c')
-            ax[1].axvline(lines_zshift[-1], c='c')
-            x_bounds = ax[1].get_xlim()
-            ax[1].annotate(text=r'H$_{beta}$', xy = (((lines_zshift[0] - x_bounds[0]) / (x_bounds[1] - x_bounds[0])), 1.01),
-                xycoords='axes fraction', verticalalignment='center', horizontalalignment='left',
-                rotation = 0)
-            ax[1].annotate(text='Magnesium b triplet', xy = (((lines_zshift[1] - x_bounds[0]) / (x_bounds[1] - x_bounds[0])), 1.01),
-                xycoords='axes fraction', verticalalignment='center', horizontalalignment='left',
-                rotation = 0, c='red')
-            ax[1].annotate(text='', xy = (((lines_zshift[2] - x_bounds[0]) / (x_bounds[1] - x_bounds[0])), 1.01),
-                xycoords='axes fraction', verticalalignment='center', horizontalalignment='left',
-                rotation = 0, c='red')
-            ax[1].annotate(text='', xy = (((lines_zshift[3] - x_bounds[0]) / (x_bounds[1] - x_bounds[0])), 1.01),
-                xycoords='axes fraction', verticalalignment='center', horizontalalignment='left',
-                rotation = 0, c='red')
-            ax[1].annotate(text='', xy = (((lines_zshift[5] - x_bounds[0]) / (x_bounds[1] - x_bounds[0])), 1.01),
-                xycoords='axes fraction', verticalalignment='center', horizontalalignment='left',
-                rotation = 0, c='m')
-            ax[1].annotate(text='Ca K', xy = (((lines_zshift[-2] - x_bounds[0]) / (x_bounds[1] - x_bounds[0])), 1.01),
-                xycoords='axes fraction', verticalalignment='center', horizontalalignment='left',
-                rotation = 0, c='c')
-            ax[1].annotate(text='Ca H', xy = (((lines_zshift[-1] - x_bounds[0]) / (x_bounds[1] - x_bounds[0])), 1.01),
-                xycoords='axes fraction', verticalalignment='center', horizontalalignment='left',
-                rotation = 0, c='c')
+            # ax[1].axvline(lines_zshift[0], c='blue')
+            # ax[1].axvline(lines_zshift[1], c='red')
+            # ax[1].axvline(lines_zshift[2], c='red')
+            # ax[1].axvline(lines_zshift[3], c='red')
+            # ax[1].axvline(lines_zshift[5], c='m')
+            # ax[1].axvline(lines_zshift[-2], c='c')
+            # ax[1].axvline(lines_zshift[-1], c='c')
+            # x_bounds = ax[1].get_xlim()
+            # ax[1].annotate(text=r'H$_{beta}$', xy = (((lines_zshift[0] - x_bounds[0]) / (x_bounds[1] - x_bounds[0])), 1.01),
+            #     xycoords='axes fraction', verticalalignment='center', horizontalalignment='left',
+            #     rotation = 0)
+            # ax[1].annotate(text='Magnesium b triplet', xy = (((lines_zshift[1] - x_bounds[0]) / (x_bounds[1] - x_bounds[0])), 1.01),
+            #     xycoords='axes fraction', verticalalignment='center', horizontalalignment='left',
+            #     rotation = 0, c='red')
+            # ax[1].annotate(text='', xy = (((lines_zshift[2] - x_bounds[0]) / (x_bounds[1] - x_bounds[0])), 1.01),
+            #     xycoords='axes fraction', verticalalignment='center', horizontalalignment='left',
+            #     rotation = 0, c='red')
+            # ax[1].annotate(text='', xy = (((lines_zshift[3] - x_bounds[0]) / (x_bounds[1] - x_bounds[0])), 1.01),
+            #     xycoords='axes fraction', verticalalignment='center', horizontalalignment='left',
+            #     rotation = 0, c='red')
+            # ax[1].annotate(text='', xy = (((lines_zshift[5] - x_bounds[0]) / (x_bounds[1] - x_bounds[0])), 1.01),
+            #     xycoords='axes fraction', verticalalignment='center', horizontalalignment='left',
+            #     rotation = 0, c='m')
+            # ax[1].annotate(text='Ca K', xy = (((lines_zshift[-2] - x_bounds[0]) / (x_bounds[1] - x_bounds[0])), 1.01),
+            #     xycoords='axes fraction', verticalalignment='center', horizontalalignment='left',
+            #     rotation = 0, c='c')
+            # ax[1].annotate(text='Ca H', xy = (((lines_zshift[-1] - x_bounds[0]) / (x_bounds[1] - x_bounds[0])), 1.01),
+            #     xycoords='axes fraction', verticalalignment='center', horizontalalignment='left',
+            #     rotation = 0, c='c')
             # ax[2].axvline(lines_zshift[0], c='blue')
             # ax[2].axvline(lines_zshift[1], c='red')
             # ax[2].axvline(lines_zshift[2], c='red')
